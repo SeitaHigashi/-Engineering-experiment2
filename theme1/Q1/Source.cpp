6646343fe2d034096e83076cb0f590522d33b2d8
SeitaHigashi <higashi110902@gmail.com>
2017-10-05 17:36:43 +0900

    first commit

---
+++ theme1/Q1/Source.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int j,i=0;
+	char c[21];
+	for(int k=0;k<21;k++)c[k]=NULL;
+	for(j=i=0;i<10;i++,j++){
+		scanf("%c%*c",&c[j]);
+	if(c[j+1]!=NULL)j++;
+	for()
+	}
+	c[j]='\0';
+
+	return 0;
+}
