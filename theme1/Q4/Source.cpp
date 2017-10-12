53b1f587752ddff221b6f6401f01893daffe6a4c
SeitaHigashi <higashi110902@gmail.com>
2017-10-12 12:57:09 +0900

    theme1Complete

---
+++ theme1/Q4/Source.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct{
+    double add,sub,multi,div,avg;
+}Anser;
+
+Anser calc(double x,double y){
+    Anser ans;
+    ans.add=x+y;
+    ans.sub=x-y;
+    ans.multi=x*y;
+    ans.div=x/y;
+    ans.avg=(x+y)/2;
+    return ans;
+}
+
+int main(){
+    Anser ans;
+    double x,y;
+    scanf("%lf %lf",&x,&y);
+    ans=calc(x,y);
+    printf("add:%lf\n",ans.add);
+    printf("sub:%lf\n",ans.sub);
+    printf("multi:%lf\n",ans.multi);
+    printf("div:%lf\n",ans.div);
+    printf("avg:%lf\n",ans.avg);
+    return 0;
+}
